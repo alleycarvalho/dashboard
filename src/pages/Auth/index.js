eb52f2163fcc809fb9a51c411c66ba31a57f6beb
Alley M. Carvalho <alleycarvalho@gmail.com>
2020-04-03 16:04:55 -0300

    feat(auth): login

--- src/pages/Auth/index.js
+++ src/pages/Auth/index.js
@@ -1,73 +1,106 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Layout, Form, Input, Button } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 import './styles.css';
 
-const Auth = () => (
-  <Layout className="login-container">
-    <section className="form">
-      <Form name="login" className="login-form">
-        <h1>Faça seu login</h1>
+import { useDispatch, useSelector } from 'react-redux';
+import { Creators as AuthActions, credentials } from '~/store/ducks/auth';
 
-        <Form.Item
-          name="username"
-          rules={[
-            {
-              required: true,
-              message: 'Preencha o e-mail!',
-            },
-          ]}
-        >
-          <Input
-            type="email"
-            size="large"
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="Seu e-mail"
-          />
-        </Form.Item>
+import history from '~/services/history';
 
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: 'Preencha a senha!',
-            },
-          ]}
-        >
-          <Input
-            type="password"
-            size="large"
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            placeholder="Sua senha"
-          />
-        </Form.Item>
+const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            size="large"
-            className="login-form-button"
+  const error = useSelector((state) => state.auth.error);
+  const token = useSelector((state) => state.auth.token);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (token) {
+      history.push('/dashboard');
+    }
+  }, [token]);
+
+  const handleLogin = () => {
+    dispatch(AuthActions.login(email, password));
+  };
+
+  return (
+    <Layout className="login-container">
+      <section className="form">
+        <Form name="login" className="login-form" onFinish={handleLogin}>
+          <h1>Faça seu login</h1>
+
+          {error && <div className="login-message">{error}</div>}
+
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: 'Preencha o e-mail!',
+              },
+            ]}
           >
-            Entrar
-          </Button>
-        </Form.Item>
-      </Form>
+            <Input
+              type="email"
+              size="large"
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Seu e-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Item>
 
-      <div className="login-info">
-        <strong>E-mail: </strong>teste@dashboard.com.br
-        <br />
-        <strong>Senha: </strong>teste
-      </div>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Preencha a senha!',
+              },
+            ]}
+          >
+            <Input
+              type="password"
+              size="large"
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              placeholder="Sua senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Item>
 
-      <div className="link-back">
-        <Link to="/">&laquo; Retornar para Home &raquo;</Link>
-      </div>
-    </section>
-  </Layout>
-);
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              size="large"
+              className="login-form-button"
+            >
+              Entrar
+            </Button>
+          </Form.Item>
+        </Form>
+
+        <div className="login-info">
+          <strong>E-mail: </strong>
+          {credentials.email}
+          <br />
+          <strong>Senha: </strong>
+          {credentials.password}
+        </div>
+
+        <div className="link-back">
+          <Link to="/">&laquo; Retornar para Home &raquo;</Link>
+        </div>
+      </section>
+    </Layout>
+  );
+};
 
 export default Auth;
